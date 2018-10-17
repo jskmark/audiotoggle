936215e979510f6d976c0bc02ace2fa8888544c0
Harishree R <harishreer@gmail.com>
2018-10-17 12:43:53 +0530

    java audio mode changed

--- src/android/com/dooble/audiotoggle/AudioTogglePlugin.java
+++ src/android/com/dooble/audiotoggle/AudioTogglePlugin.java
@@ -34,7 +34,7 @@ public class AudioTogglePlugin extends CordovaPlugin {
 	    
 	    if (mode.equals("earpiece")) {
 	    	audioManager.setMode(AudioManager.MODE_IN_COMMUNICATION);
-	    	audioManager.setSpeakerphoneOn(false);
+	    	audioManager.setSpeakerphoneOn(true);
 	        return true;
 	    } else if (mode.equals("speaker")) {        
 	    	audioManager.setMode(AudioManager.STREAM_MUSIC);
@@ -42,11 +42,11 @@ public class AudioTogglePlugin extends CordovaPlugin {
 	        return true;
 	    } else if (mode.equals("ringtone")) {        
 	    	audioManager.setMode(AudioManager.MODE_RINGTONE);
-	    	audioManager.setSpeakerphoneOn(false);
+	    	audioManager.setSpeakerphoneOn(true);
 	        return true; 
 	    } else if (mode.equals("normal")) {        
 	    	audioManager.setMode(AudioManager.MODE_NORMAL);
-	    	audioManager.setSpeakerphoneOn(false);
+	    	audioManager.setSpeakerphoneOn(true);
 	        return true;
 	    }
 	    
